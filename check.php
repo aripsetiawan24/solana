9cb0261b8554a50e1124c28f7334ea1c7162b090
Arip Setiawan <35103594+aripsetiawan24@users.noreply.github.com>
2021-08-23 14:17:33 +0700

    Update check.php

--- check.php
+++ check.php
@@ -3,7 +3,11 @@ echo "\e[32m1.LIQ";
 echo "\n2.BOP";
 echo "\n3.COPE";
 echo "\n4.ROPE";
-echo "\n5.APEX\e[39m";
+echo "\n5.APEX";
+echo "\n6.GUPPY";
+echo "\n7.MOLA";
+echo "\n8.SOLAPE";
+echo "\n9.SAMO\e[39m";
 echo "\n\e[33mPilih wallet yang ingin di check :\e[39m";
 $pilihan = trim(fgets(STDIN));
 if ($pilihan == "1"){
@@ -35,5 +39,29 @@ if ($pilihan == "5"){
     echo "\n";
     sleep(1);
      require "src/apex.php";
+}
+if ($pilihan == "6"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/guppy.php";
+}
+if ($pilihan == "7"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/mola.php";
+}
+if ($pilihan == "8"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/solape.php";
+}
+if ($pilihan == "9"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/samo.php";
 }
 ?>

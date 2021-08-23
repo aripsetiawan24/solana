7fd7602cfb27af62ecca659a18d513437affd62c
Arip Setiawan <35103594+aripsetiawan24@users.noreply.github.com>
2021-08-23 14:10:00 +0700

    Update check.php

--- check.php
+++ check.php
@@ -1,6 +1,9 @@
 <?php 
 echo "\e[32m1.LIQ";
-echo "\n2.BOP\e[39m";
+echo "\n2.BOP";
+echo "\n3.COPE";
+echo "\n4.ROPE";
+echo "\n5.APEX\e[39m";
 echo "\n\e[33mPilih wallet yang ingin di check :\e[39m";
 $pilihan = trim(fgets(STDIN));
 if ($pilihan == "1"){
@@ -14,5 +17,23 @@ if ($pilihan == "2"){
     echo "\n";
     sleep(1);
      require "src/bop.php";
+}
+if ($pilihan == "3"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/cope.php";
+}
+if ($pilihan == "4"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/rope.php";
+}
+if ($pilihan == "5"){
+    echo "Silahkan Tunggu";
+    echo "\n";
+    sleep(1);
+     require "src/apex.php";
 }
 ?>
